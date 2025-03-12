999a2799eeed88541304cc82c69c21c0ef13a541
Sung Y Kim <37357335+tetron225@users.noreply.github.com>
2025-03-12 19:41:28 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/template.html",
+    }),
+  ]
+};
